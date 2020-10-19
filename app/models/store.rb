211d2bb00e416f75c31080809365fd92475ba208
gianniresende <gianniresende@gmail.com>
2020-10-19 18:30:55 -0300

    refactor: model store

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,18 @@
 class Store < ApplicationRecord
+
+  #relations
+
   has_many :ratings
   validates_presence_of :lonlat, :name, :google_place_id
   validates :google_place_id, uniqueness: true
+
+  #scopes
+
+  scope :within, -> (longitude, latitude, distance_in_km = 5) {
+    where(%{
+      ST_Distance(lonlat, 'POINT(%f %f)') < %d
+    } % [longitude, latitude, distance_in_km * 1000])
+  }
+
+
 end
